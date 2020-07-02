8776da858cb7cb745d3a7cd9737a60175bc4eaec
RafaelMorais <rafaelmorais.design@gmail.com>
2020-07-02 20:54:59 -0300

    Iniciando o front da aula de laravel

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -9,13 +9,12 @@ class CardsController extends Controller
 {
     public function index() {
         // obtendo todos os registros da tabela cards
-        $cards = [
-            "Card 1", "Card 2", "Card 3", "Card 4", 
-            "Card 5", "Card 6", "Card 7", "Card 8" 
-        ];
+        $cards = Card::all();
     
+        // verificando se obteve registros para listar
         if ($cards) {
-            return view('welcome')->with(['cards' => $cards]);
+            // retornando resposta JSON com todos os cards encontrados
+            return view('cards.index')->with(['cards', $cards]);
         }
     }
 
